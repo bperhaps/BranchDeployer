3015cb1f958b23abb2fc07e89dc824901f7cb1fc
Minsung Son <sns2831@gmail.com>
2021-09-29 13:42:04 +0900

    Chore: Initial commit

---
+++ src/main/java/kr/co/techcourse/prolog/branshdeployer/BranshDeployerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.techcourse.prolog.branshdeployer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranshDeployerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranshDeployerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/techcourse/prolog/branshdeployer/BranshDeployerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.techcourse.prolog.branshdeployer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranshDeployerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
